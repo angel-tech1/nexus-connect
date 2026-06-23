7b50758ac68c0b09d765e8a33e66f2cc263a01af
angel-tech1 <117034253+angel-tech1@users.noreply.github.com>
2026-06-23 17:06:59 -0600

    Kotlin project setup

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
